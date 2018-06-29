c194d4dcb48cc2148adec58dd2ad2d689c81653d
eyebis <danataylor.ct@gmail.com>
2018-06-29 11:00:43 -0400

    added functionality to output to csv file with -o option

--- aws-recon.py
+++ aws-recon.py
@@ -10,11 +10,59 @@ contents = ""
 parser = argparse.ArgumentParser(description='perform aws reconnaissance via api scraping')
 parser.add_argument("credsfile", help="specify whether to read creds from the 'credentials' or 'config' file found in ~./aws/")
 parser.add_argument("-p", "--public", action="store_true", help="use to only extract information about objects that are internet-facing/publicly accessible. without this flag, only internal, privately addressed resources will be returned")
+parser.add_argument("-o", "--output", help="use --output <file.csv> to write. defaults to stdout")
 args = parser.parse_args()
 
+def pull_elb (sesh, writer): # Elastic Load Balancers
+	client = sesh.client('elb')
+	response = client.describe_load_balancers()
+	for item in response["LoadBalancerDescriptions"]:
+		dns = item["DNSName"]
+		if args.public:
+			if "internal" not in dns:
+				output(writer,dns)
+		else:
+			if "internal" in dns:
+				output(writer,dns)
+
+def pull_alb (sesh, writer): # Application Load Balancers
+	client = sesh.client('elbv2')
+	response = client.describe_load_balancers()
+	for item in response["LoadBalancers"]:
+		dns = item["DNSName"]
+		if args.public:
+			if "internal" not in dns:
+				output(writer,dns)
+		else:
+			if "internal" in dns:
+				output(writer,dns)
+
+def pull_eip (sesh, writer): # External Elastic IP Addresses
+	client = sesh.client('ec2')
+	response = client.describe_addresses()
+	for item in response["Addresses"]:
+		pub = item["PublicIp"]
+		output(writer,pub)
+
+def pull_pip (sesh, writer): # Internal EC2 IP Addresses
+	client = sesh.client('ec2')
+	response = client.describe_instances()
+	for reserv in response["Reservations"]:
+		for instance in reserv["Instances"]:
+			ec2r = sesh.resource('ec2')
+			inst = ec2r.Instance(instance["InstanceId"])
+			priv = inst.private_ip_address
+			output(writer,priv)
+
+def output (writer,item):
+	if args.output:
+		writer.writerow({'Target': item})
+	else:
+		print(item)
+
 def extractProfiles(home, args, profiles, contents):
-	f = open(home+"/.aws/"+args.credsfile,"r")
-	if f.mode == 'r': contents = f.read(); f.close()
+	cf = open(home+"/.aws/"+args.credsfile,"r")
+	if cf.mode == 'r': contents = cf.read(); cf.close()
 	if args.credsfile == "config":
 		if "default" in contents: profiles.append("default")
 		for i in re.findall("(?:profile).*\w",contents): profiles.append(i.split(' ')[1])
@@ -30,58 +78,23 @@ except PermissionError:
 	print("you don't have permission to access",args.credsfile)
 	exit()
 
-def pull_elb (sesh): # Elastic Load Balancers
-	client = sesh.client('elb')
-	response = client.describe_load_balancers()
-	for item in response["LoadBalancerDescriptions"]:
-		dns = item["DNSName"]
-		if args.public:
-			if "internal" not in dns:
-				print(dns)
-		else:
-			if "internal" in dns:
-				print(dns)
-
-def pull_alb (sesh): # Application Load Balancers
-	client = sesh.client('elbv2')
-	response = client.describe_load_balancers()
-	for item in response["LoadBalancers"]:
-		dns = item["DNSName"]
-		if args.public:
-			if "internal" not in dns:
-				print(dns)
-		else:
-			if "internal" in dns:
-				print(dns)
-
-def pull_eip (sesh): # External Elastic IP Addresses
-	client = sesh.client('ec2')
-	response = client.describe_addresses()
-	for item in response["Addresses"]:
-		pub = item["PublicIp"]
-		print(pub)
-
-def pull_pip (sesh): # Internal EC2 IP Addresses
-	client = sesh.client('ec2')
-	response = client.describe_instances()
-	for reserv in response["Reservations"]:
-		for instance in reserv["Instances"]:
-			ec2r = sesh.resource('ec2')
-			inst = ec2r.Instance(instance["InstanceId"])
-			priv = inst.private_ip_address
-			print(priv)
+if args.output:
+	of = open(args.output,"w",newline='')
+	writer = csv.DictWriter(of, fieldnames=['Target'])
+else:
+	writer = ""
 
 for acct in profiles:
 	for az in regions:
 		print("profile:",acct,"-- region:",az)
 		sesh = boto3.session.Session(region_name = az, profile_name = acct)
 		try:
-			pull_elb(sesh)
-			pull_alb(sesh)
+			pull_elb(sesh,writer)
+			pull_alb(sesh,writer)
 			if args.public:
-				pull_eip(sesh)
+				pull_eip(sesh,writer)
 			else:
-				pull_pip(sesh)
+				pull_pip(sesh,writer)
 		except ClientError as err:
 			if "InvalidClientTokenId" in str(err):
 				print("invalid credentials")
